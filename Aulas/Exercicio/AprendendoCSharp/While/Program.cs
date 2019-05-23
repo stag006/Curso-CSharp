34e5022a149a931512ba8eaa8db93e8992015e67
Arthur teixeira <stag006@benner.com.br>
2019-05-23 12:02:21 -0300

    enviando os exercicios feitos na benner

---
+++ Aulas/Exercicio/AprendendoCSharp/While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 9;
+
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador = contador + 10;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
