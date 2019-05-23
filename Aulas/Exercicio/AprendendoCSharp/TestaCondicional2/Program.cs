7bffa7ce1b86df0f754c412a443351840bd1acc4
Arthur Teixeira <stag006@benner.com.br>
2019-05-23 01:07:23 -0300

    Enviando os execicios para o repositorio

---
+++ Aulas/Exercicio/AprendendoCSharp/TestaCondicional2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Agora vou estar usando o comando (bool). Portanto!
+
+            int idade = 19;
+            int quantidadePessoas = 3;
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idade >= 18 && acompanhado)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar");
+            }
+        }
+    }
+}
