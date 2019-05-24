8d88e2fc5a67d880773fb89b76053751d9111452
Arthur teixeira <stag006@benner.com.br>
2019-05-24 10:26:30 -0300

    LacoDeRepeticaoFor123

--- Aulas/Exercicio/AprendendoCSharp/1-while/Program.cs
+++ Aulas/Exercicio/AprendendoCSharp/1-while/Program.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LacoDeRepeticaoWhile
+namespace _1_while
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int contador = 0;
-            while (contador <= 10) ;
+            Console.WriteLine("Executando a  Variavel While");
+            Console.WriteLine();
+
+            int contador = 1;
+            while (contador <=10)
             {
                 Console.WriteLine(contador);
-                contador++;
+                contador ++; //     Que é a mesma coisa do que: contador +=1;
             }
-
+            
             Console.ReadLine();
         }
     }
